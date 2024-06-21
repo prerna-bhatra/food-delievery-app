b111b5ca9ad01c00f85fa01a248b81dd23a984e9
prernaorangeleague <prerna@orangeleague.in>
2024-06-21 10:15:18 +0530

    pg

--- db/index.js
+++ db/index.js
@@ -1,27 +1,28 @@
 const { Sequelize, DataTypes } = require('sequelize');
-
+const pg = require("pg")
 
 // console.log({db:process.env.DB_HOST});
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host:  process.env.DB_HOST,
-      dialect:  require('pg'),
-      port: '50013',
-      // dialectOptions: {
-      //   ssl: {
-      //     require: true, // This will help you. But you will see nwe error
-      //     rejectUnauthorized: false // This line will fix new error
-      //   }
-      // },   
-     }
-  );
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: '50013',
+    dialect: "postgres",
+    dialectModule: pg,
+    // dialectOptions: {
+    //   ssl: {
+    //     require: true, // This will help you. But you will see nwe error
+    //     rejectUnauthorized: false // This line will fix new error
+    //   }
+    // },   
+  }
+);
 
-  // console.log({sequelize});
+// console.log({sequelize});
 
 module.exports = {
-    sequelize
+  sequelize
 };
