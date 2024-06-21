c4c7c2d2f39298134db5f55b02c63e890a2432a5
prernaorangeleague <prerna@orangeleague.in>
2024-06-21 09:38:32 +0530

    pg

--- db/index.js
+++ db/index.js
@@ -9,7 +9,7 @@ const sequelize = new Sequelize(
     process.env.DB_PASSWORD,
     {
       host:  process.env.DB_HOST,
-      dialect:  'postgres',
+      dialect:  'pg',
       port: '50013',
       // dialectOptions: {
       //   ssl: {
